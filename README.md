c4070abffaa97dd9440b6198e2cf442dbc51dd23
Hamed Asemi <h-@live.se>
2017-01-24 12:32:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+## Install
+
+```sh
+npm install webrew-helpers-env --save-dev
+```
+
+## Usage
+
+```javascript
+
+import { ENV } from 'webrew-helpers-debug'
+
+console.log(`${typeof ENV}:, ${ENV}`)
+
+```
---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var ENV = exports.ENV = typeof process.env.ENV !== "undefined" ? false : process.env.ENV;
+
+var _module = {
+    ENV: ENV
+};
+
+exports.default = _module;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL2luZGV4LmpzIl0sIm5hbWVzIjpbIkVOViIsInByb2Nlc3MiLCJlbnYiLCJtb2R1bGUiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sSUFBTUEsb0JBQU0sT0FBT0MsUUFBUUMsR0FBUixDQUFZRixHQUFuQixtQkFBeUMsS0FBekMsR0FBaURDLFFBQVFDLEdBQVIsQ0FBWUYsR0FBekU7O0FBRVAsSUFBSUcsVUFBUztBQUNUSDtBQURTLENBQWI7O2tCQUllRyxPIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IEVOViA9IHR5cGVvZiBwcm9jZXNzLmVudi5FTlYgIT09IGB1bmRlZmluZWRgID8gZmFsc2UgOiBwcm9jZXNzLmVudi5FTlZcblxubGV0IG1vZHVsZSA9IHtcbiAgICBFTlZcbn1cblxuZXhwb3J0IGRlZmF1bHQgbW9kdWxlIl19
